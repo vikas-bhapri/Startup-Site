0b275a3bb20fe133d7f8c8af005ed8549e29f097
Vikas Bhapri <vikasbhapri@gmail.com>
2024-11-03 22:03:08 +0530

    Did changes according to the medium article

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode:true,
+  distDir: 'build',
+  output:'standalone',
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [{
